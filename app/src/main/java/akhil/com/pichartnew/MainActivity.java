f4653202ed75b0b260f8261c5120d7c489431399
fattahaabdul <fattah@xipetech.com>
2018-08-11 10:42:36 +0530

    This is a example of pie chart

---
+++ app/src/main/java/akhil/com/pichartnew/MainActivity.java
@@ -0,0 +1,65 @@
+package akhil.com.pichartnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.EnumsAlign;
+import com.anychart.anychart.LegendLayout;
+import com.anychart.anychart.Pie;
+import com.anychart.anychart.ValueDataEntry;
+import com.anychart.anychart.chart.common.Event;
+import com.anychart.anychart.chart.common.ListenersInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+        Pie pie = AnyChart.pie();
+
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(MainActivity.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Apples", 6371664));
+        data.add(new ValueDataEntry("Pears", 789622));
+        data.add(new ValueDataEntry("Bananas", 7216301));
+        data.add(new ValueDataEntry("Grapes", 1486621));
+        data.add(new ValueDataEntry("Oranges", 1200000));
+
+        pie.setData(data);
+
+        pie.setTitle("Fruits imported in 2015 (in kg)");
+
+        pie.getLabels().setPosition("outside");
+
+        pie.getLegend().getTitle().setEnabled(true);
+        pie.getLegend().getTitle()
+                .setText("Retail channels")
+                .setPadding(0d, 0d, 10d, 0d);
+
+        pie.getLegend()
+                .setPosition("center-bottom")
+                .setItemsLayout(LegendLayout.HORIZONTAL)
+                .setAlign(EnumsAlign.CENTER);
+
+        anyChartView.setChart(pie);
+
+    }
+}
